d1b4c63990415c73adcf53f33c889ae32c3ea105
HanxiChen <71202369+HanxiChen@users.noreply.github.com>
2020-09-13 13:45:01 +0800

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,59 @@
+ex1
+/*
+#include<stdio.h>
+int main()
+{
+	printf("³ÂººÎõ 191543210\n");
+	return 1;
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+	printf("* * * * \n");
+	printf(" * * * \n");
+	printf("  * * \n");
+	printf("   *\n");
+	return 1;
+}
+*/
+ 
+/*
+#include<stdio.h>
+int main()
+{
+	printf("Welcome to GDUF!\n");
+	return 1;
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+	printf("*****************\n");
+	printf("Please Stduy Hard\n");
+	printf("*****************\n");
+    return 1;
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+	printf("I Love Programming£¡\n");
+    return 1;
+}
+*/
+
+/* 
+#include<stdio.h>
+int main()
+{ 
+    printf("Hello everybody!\n");
+	return 1;
+}
+*/
